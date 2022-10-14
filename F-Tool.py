e803785a816ae27054a266469b791bfcdbb650cf
Đậu Đậu <59077968+DauDau432@users.noreply.github.com>
2022-10-14 19:00:06 +0700

    Update F-Tool.py

--- F-Tool.py
+++ F-Tool.py
@@ -62,11 +62,12 @@ class Home:
 	def home(self): # don't edit this banner lol
 		print(f"""
                         {Color.LG}╔════════════════════════╗
-    {Color.LC}╔═╗{Color.LB} ╔╦╗╔═╗╔═╗╦      {Color.LG}║ {Color.LR}Created: {Color.LY}5/3/22    {Color.LG}║
-    {Color.LC}╠╣{Color.LB}{Color.LR}───{Color.LB}║ ║ ║║ ║║      {Color.LG}║ {Color.LR}Updated: {Color.LY}8/3/22    {Color.LG}║
+    {Color.LC}╔═╗{Color.LB} ╔╦╗╔═╗╔═╗╦      {Color.LG}║ {Color.LR}Created: {Color.LY}5/3/22        {Color.LG}║
+    {Color.LC}╠╣{Color.LB}{Color.LR}───{Color.LB}║ ║ ║║ ║║      {Color.LG}║ {Color.LR}Updated: {Color.LY}8/3/22        {Color.LG}║
     {Color.LC}╚{Color.LB}    ╩ ╚═╝╚═╝╩═╝{Color.LG}v2  {Color.LG}║ {Color.LB}Đơn giản nhưng mạnh mẽ {Color.LG}║
                         {Color.LG}╚════════════════════════╝
-    {Color.LR}[{Color.LG}> Được thực hiện bởi FDc0d3 {Color.LG}<{Color.LR}]""")
+    {Color.LR}[{Color.LG}> Được thực hiện bởi FDc0d3 {Color.LG}<{Color.LR}]
+    {Color.LR}[{Color.LG}>    Được dịch bởi Đậu Đậu    {Color.LG}<{Color.LR}]""")
 		print(Color.LC+"    Gõ "+Color.LB+"'HELP'"+Color.LC+" để xem tất cả các lệnh\n\n")
 		print(Color.LR+"["+Color.LG+"01"+Color.LR+"]"+Color.LC+" Proxy")
 		print(Color.LR+"["+Color.LG+"02"+Color.LR+"]"+Color.LC+" WebTool")
